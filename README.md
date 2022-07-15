91e8ff29cff47c500c68f46c5878a675031596d6
Mahmoud Ayman <m7m0udayman@gmail.com>
2022-07-15 23:37:33 +0200

    add scroll into section feat and make navigation responsive

--- README.md
+++ README.md
@@ -1,13 +1,40 @@
 # Landing Page Project
 
-## Table of Contents
+first I linked Html with JS files.
 
-* [Instructions](#instructions)
+- I divide the project requirements into parts:
 
-## Instructions
+        1. Build dynamic navigation bar for sections with Js.
+- to create navbar I used a fragment.
+- I used for loop to create navbar items and then append it to the fragment. 
+- create element li and then by backtick I created inside it an anchor tag with href that I get by id name of the section.
+- create text content inside the anchor tag by using text inside data-nav attribute.
+- append the li to the fragment.
+- append the fragment to the navbar.
+       
+        2. add class active to section when newa top of viewport.
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+- for this part I select all navbar items by querSelectorAll method.
+- and used IntersectionObserver to observe the top of viewport.
+- by threshold I set the top of viewport to 0.7 that means when 70% of the element inside the viewport class active will be added.
+- I used for Each method to add class active to the element.
+- and to add active class to the navbar item that is related to the current section shown in viewport.
 
-To get started, open `js/app.js` and start building out the app's functionality
+    - I create a variable.
+    - in this variable I get the data-nav attribute value of the current section shown in viewport.
+    - I used split method to get the value of the number in data-nav attribute and this will be the index of the navbar item in navBarItems list that I created before that will be added class active.
+- I used forEach method to observe every section in sections node list.
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+        3. Scroll to anchor ID using JavaScript.
+
+- first I used forEach method to loop on each navbar item.
+- I used addEventListener method to add event listener to the navbar item with event.
+- I used preventDefault method to prevent the default action of the event.
+- I used variable called index to get the id of section that I want to scroll to by using getAttributeNode and passing "href" cause when I declared those navbar Items earlier I give each anchor tag the id of the section that is related for.
+- I used scrollIntoView method to scroll to the section that has the same id as the navbar item.
+
+---
+I make a responsive navigation bar that appears in phone screen size.
+- I used a media query to make the navigation bar appear and disappear in phone screen size.
+- I declared a new class called "show" to control the display of the header.
+- by JS I add a class show to the header when a click event happens to the toggler I have created.
--- js/app.js
+++ js/app.js
@@ -1,70 +1,37 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
 let navbarList = document.getElementById("navbar__list");
 let sections = document.querySelectorAll("section");
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
+// build the dynamic navbar
 
 const fragment = document.createDocumentFragment();
-sections.forEach((section) => {
-  const navbarItem = document.createElement("li");
-  navbarItem.innerHTML = `<a href="#${
-    section.getAttributeNode("id").value
-  }" class="menu__link">${section.getAttributeNode("data-nav").value}</a>`;
-  fragment.appendChild(navbarItem);
-});
-navbarList.appendChild(fragment);
+function createNavbar() {
+  sections.forEach((section) => {
+    const navbarItem = document.createElement("li");
+    navbarItem.innerHTML = `<a href="#${
+      section.getAttributeNode("id").value
+    }" class="menu__link">${section.getAttributeNode("data-nav").value}</a>`;
+    fragment.appendChild(navbarItem);
+  });
+  navbarList.appendChild(fragment);
+}
+createNavbar();
 
 // Add class 'active' to section when near top of viewport
 let navBarItems = document.querySelectorAll("li a");
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          //add class to section
-            entry.target.classList.add("active");
-            //add class to navbar link
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        //add class to navbar link that is related to the active section
+        // by using data-nav attributeand split it I can get the number of the section 
+        // and use it as an index in navBarItems list.
         let index = entry.target
           .getAttributeNode("data-nav")
           .value.split(" ")[1];
         navBarItems[index - 1].classList.add("active");
-        } else {
-            //remove class from the section
-            entry.target.classList.remove("active");
-            //remove class from the navbar link 
+      } else {
+        entry.target.classList.remove("active");
         let index = entry.target
           .getAttributeNode("data-nav")
           .value.split(" ")[1];
@@ -72,37 +39,35 @@ const observer = new IntersectionObserver(
       }
     });
   },
-    {
-      // when 70% of elemen inside the viewport class active added or removed 
+  {
     threshold: 0.7,
   }
 );
-// observe work on one element only so loop 
+
 sections.forEach((section) => {
   observer.observe(section);
 });
+
 // Scroll to anchor ID using scrollTO event
 
 navBarItems.forEach((navbarItem) => {
   navbarItem.addEventListener("click", function (event) {
     event.preventDefault();
     const index = navbarItem.getAttributeNode("href").value;
+    console.log(index);
     let section = document.querySelector(index);
-    console.log(section);
-    window.scrollTo(
-      section.getBoundingClientRect().x,
-      section.getBoundingClientRect().y
-    );
+    section.scrollIntoView({ behavior: "smooth" });
   });
 });
-/**
- * End Main Functions
- * Begin Events
- *
- */
 
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+//make responsive navigation bar
+let navbarToggler = document.querySelector(".navbar__toggle");
+let pageHeader = document.querySelector(".page__header");
+navbarToggler.addEventListener("click", function navigationMenu(event) {
+  pageHeader.classList.toggle("show");
+  navbarToggler.classList.toggle("activeNav");
+  if (!navbarToggler.contains(event.target)) {
+    pageHeader.classList.remove("show");
+    navbarToggler.classList.remove("activeNav");
+  }
+});
